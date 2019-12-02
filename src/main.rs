7d0e7abe1e3018bb75ab5ffea6ef5db20c49c7f8
Alex Krivolutsky <irramail@gmail.com>
2019-12-03 03:04:11 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+extern crate redis;
+
+use redis::{Client, Commands, Connection, RedisResult};
+use jsonrpc_http_server::jsonrpc_core::{IoHandler, Value, Params, Error};
+use jsonrpc_http_server::{ServerBuilder};
+
+fn parse_arguments (p: Params) -> Result<Vec<String>, Error> {
+    let mut result = Vec::new();
+    match p {
+        Params::Array(array) => {
+            for s in &array {
+                match s {
+                    Value::String(s) => result.push(s.clone()),
+                    _ => return Err(Error::invalid_params("expecting strings"))
+                }
+            }
+        }
+        _ => return Err(Error::invalid_params("expecting an array of strings"))
+    }
+    if result.len() < 1 {
+        return Err(Error::invalid_params("missing api key"));
+    }
+
+    return Ok(result[0..].to_vec());
+}
+
+fn fetch_an_integer(id: &str, ver: &str) -> redis::RedisResult<isize> {
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut con = client.get_connection()?;
+    let _ : () = con.set(id, ver )?;
+
+    con.get("my_key")
+}
+
+fn main() {
+    let mut io = IoHandler::new();
+    io.add_method("openssl_version", move |params: Params| {
+        let w = parse_arguments(params)?;
+        let _ = fetch_an_integer( &w[0], &w[1]);
+        Ok(Value::String(w.join("-").to_string()))
+    });
+
+    let server = ServerBuilder::new(io)
+        .threads(3)
+        .start_http(&"127.0.0.1:3030".parse().unwrap())
+        .unwrap();
+
+    server.wait();
+}
