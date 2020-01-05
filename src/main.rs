0b6fc6dcac0a701e2f0ea3ae7ca07bc2f8222b1f
Alex Krivolutsky <irramail@gmail.com>
2020-01-05 23:22:05 +0700

    clean println

--- src/main.rs
+++ src/main.rs
@@ -77,7 +77,6 @@ fn get_a_stat() -> redis::RedisResult<String>  {
 
     let mut keys: Vec<String> = con.keys("*_html")?;
     keys.sort();
-    println!("{:?}", keys);
     let mut uniq_html = String::new();
     for _ in 1..100 {
         let content: String = con.get(keys.pop().unwrap())?;
